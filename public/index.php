2b49542889c8230d3fd08be09fa92ef1129e2974
Benjamin Ortiz <bortiz@transmandu.com>
2025-07-25 10:19:44 -0400

    prueba

--- public/index.php
+++ public/index.php
@@ -1,20 +1,32 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Modo mantenimiento
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Autoload Composer
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
+
+// Captura la petición HTTP
+$request = Request::capture();
+
+// Maneja la petición y obtiene respuesta
+$response = $kernel->handle($request);
+
+// Envía la respuesta al navegador
+$response->send();
+
+// Finaliza la petición
+$kernel->terminate($request, $response);
